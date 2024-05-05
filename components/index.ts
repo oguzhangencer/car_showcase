aaab9cfba1fd29993b3ec921eb68754246272f99
Oguzhan Gencer <gencer7399@gmail.com>
2024-05-05 21:23:53 +0300

    feat: Add CustomFilter and SearchBar components

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,18 @@
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManuFacturer from "./SearchManuFacturer";
+
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    CustomFilter,
+    SearchBar,
+    Footer,
+    SearchManuFacturer
 }
