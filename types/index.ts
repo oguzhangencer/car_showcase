510d0445ee8f72a77230922584a85ca69fbe5b2d
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-05-06 23:47:02 +0300

    refactor: Add CarProps and FilterProps interfaces

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,40 @@ export interface SearchManuFacturerProps {
     manuFacturer: string;
     setManuFacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
+  
+  export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+  }
+
+  export interface HomeProps {
+    searchParams: FilterProps;
+  }
+  
+  export interface CarCardProps {
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
+  }
