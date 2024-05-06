52ad475896d038ccc36ebad0abe63637fb554081
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-05-07 01:39:18 +0300

    refactor: Improve SearchManuFacturer component performance and fix transition bug

--- components/SearchManuFacturer.tsx
+++ components/SearchManuFacturer.tsx
@@ -27,22 +27,24 @@ export default function SearchManuFacturer({
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
-              src="./car-logo.svg"
-              alt="Car Logo"
-              className="ml-4"
+              src="/car-logo.svg"
               width={20}
               height={20}
+              className="ml-4"
+              alt="car logo"
             />
           </Combobox.Button>
+
           <Combobox.Input
             className="search-manufacturer__input"
-            placeholder="Volkswagen"
-            displayValue={(manufacturer: string) => manufacturer}
-            onChange={(event) => setQuery(event.currentTarget.value)}
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder="Volkswagen..."
           />
+
           <Transition
             as={Fragment}
-            leave="transition eas-in duration-100"
+            leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
@@ -51,36 +53,48 @@ export default function SearchManuFacturer({
               className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
               static
             >
-              {filteredManufacturers.map((item) => (
+              {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
-                  key={item}
-                  className={({ active }) => `
-                      relative search-manufacturer__option ${
-                        active ? "bg-primary-blue text-white" : "text-gary-900"
-                      }
-                      `}
-                  value={item}
+                  value={query}
+                  className="search-manufacturer__option"
                 >
-                  {({ selected, active }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? "font-medium " : "font-normal"
-                        }`}
-                      >
-                        {item}
-                      </span>
-                      {selected ? (
-                        <span
-                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                            active ? "text-white" : "text-teal-600"
-                          }`}
-                        ></span>
-                      ) : null}
-                    </>
-                  )}
+                  Create "{query}"
                 </Combobox.Option>
-              ))}
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {item}
+                        </span>
+
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active
+                                ? "text-white"
+                                : "text-pribg-primary-purple"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
             </Combobox.Options>
           </Transition>
         </div>
